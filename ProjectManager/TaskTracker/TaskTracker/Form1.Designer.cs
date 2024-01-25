c38428462f7a0b96ffaee2c361b7cab1074c499e
Anbarasu <79371061+Anbarasu1715@users.noreply.github.com>
2024-01-25 16:09:58 +0530

    Add files via upload

---
+++ ProjectManager/TaskTracker/TaskTracker/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace TaskTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cards1 = new TaskTracker.Cards();
+            this.SuspendLayout();
+            // 
+            // cards1
+            // 
+            this.cards1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.cards1.Location = new System.Drawing.Point(42, 32);
+            this.cards1.Name = "cards1";
+            this.cards1.Padding = new System.Windows.Forms.Padding(8);
+            this.cards1.Size = new System.Drawing.Size(270, 406);
+            this.cards1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 450);
+            this.Controls.Add(this.cards1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Cards cards1;
+    }
+}
+
---
+++ ProjectManager/TaskTracker/TaskTracker/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskTracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            formSize = new Rectangle(Location,Size);
+
+            foreach (Control c in Controls) {
+                formConotrols.Add(new Rectangle(c.Location,c.Size));
+            }
+        }
+
+
+        private Rectangle formSize;
+        private List<Rectangle> formConotrols=new List<Rectangle>();
+
+        private void resizeControl(Control control, Rectangle rec)
+        {
+            float XRatio = (float)this.Size.Width / (float)formSize.Width;
+            float YRatio = (float)this.Size.Height / (float)formSize.Height;
+
+            int newX = (int)(rec.X * XRatio);
+            int newY = (int)(rec.Y * YRatio);
+
+            int newWidth = (int)(rec.Width * XRatio);
+            int newHeight = (int)(rec.Height * YRatio);
+
+            control.Location = new System.Drawing.Point(newX, newY);
+            control.Size = new System.Drawing.Size(newWidth, newHeight);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            int index = 0;
+            foreach (Control c in Controls) {
+                resizeControl(c, formConotrols[index++]);
+            }
+        }
+    }
+}
